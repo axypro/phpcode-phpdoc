bb468d6063189cf625d5a249a1667293d4fea4ae
Oleg Grigoriev <go.vasac@gmail.com>
2015-02-05 17:51:03 +0300

    annotations\Blank: Blank of an annotation

---
+++ annotations/Blank.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package axy\phpcode\phpdoc
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+
+namespace axy\phpcode\phpdoc\annotations;
+
+/**
+ * Blank of an annotation
+ *
+ * For example: @+tag(data) text
+ */
+class Blank
+{
+    /**
+     * @var string
+     */
+    public $tag;
+
+    /**
+     * @var string|null
+     */
+    public $data;
+
+    /**
+     * @var string
+     */
+    public $text;
+
+    /**
+     * Seeks annotation in a line
+     *
+     * @param string $line
+     * @return \axy\phpcode\phpdoc\annotations\Blank|null
+     */
+    public static function seekInLine($line)
+    {
+        if (preg_match('/^@(\S+)(.*?)$/s', $line, $matches)) {
+            return new self($matches[1], trim($matches[2]));
+        }
+        if (substr($line, 0, 13) === '{@inheritdoc}') {
+            return new self('inheritdoc', trim(substr($line, 13)));
+        }
+        return null;
+    }
+
+    /**
+     * The constructor
+     *
+     * @param string $annotation
+     * @param string $text
+     */
+    public function __construct($annotation, $text)
+    {
+        if (preg_match('/^([^(]+)\((.*?)\)$/', $annotation, $matches)) {
+            $this->tag = $matches[1];
+            $this->data = $matches[2];
+        } else {
+            $this->tag = $annotation;
+        }
+        $this->text = $text;
+    }
+
+    /**
+     * @param string $line
+     */
+    public function appendLine($line)
+    {
+        $this->text .= "\n".$line;
+    }
+}
---
+++ tests/annotations/BlankTest.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @package axy\phpcode\phpdoc
+ * @author Oleg Grigoriev <go.vasac@gmail.com>
+ */
+
+namespace axy\phpcode\phpdoc\tests;
+
+use axy\phpcode\phpdoc\annotations\Blank;
+
+/**
+ * coversDefaultClass axy\phpcode\phpdoc\annotations\Blank
+ */
+class BlankTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * covers ::seekInLine
+     * @dataProvider providerSeekInLine
+     * @param string $line
+     * @param array|null $expected
+     */
+    public function testSeekInLine($line, $expected)
+    {
+        $blank = Blank::seekInLine($line);
+        if ($expected === null) {
+            $this->assertNull($blank);
+        } else {
+            $this->assertInstanceOf('axy\phpcode\phpdoc\annotations\Blank', $blank);
+            $this->assertEquals($expected, (array)$blank);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function providerSeekInLine()
+    {
+        return [
+            [
+                '',
+                null,
+            ],
+            [
+                'Text',
+                null,
+            ],
+            [
+                'Text @link',
+                null,
+            ],
+            [
+                ' @link',
+                null,
+            ],
+            [
+                '@source',
+                [
+                    'tag' => 'source',
+                    'data' => null,
+                    'text' => '',
+                ],
+            ],
+            [
+                '@param string $x [optional]',
+                [
+                    'tag' => 'param',
+                    'data' => null,
+                    'text' => 'string $x [optional]',
+                ],
+            ],
+            [
+                '@property-read string $prop description',
+                [
+                    'tag' => 'property-read',
+                    'data' => null,
+                    'text' => 'string $prop description',
+                ],
+            ],
+            [
+                '@my\Annotation(x=1;y=2) text ',
+                [
+                    'tag' => 'my\Annotation',
+                    'data' => 'x=1;y=2',
+                    'text' => 'text',
+                ],
+            ],
+            [
+                '{@see}',
+                null,
+            ],
+            [
+                '{@inheritdoc}',
+                [
+                    'tag' => 'inheritdoc',
+                    'data' => null,
+                    'text' => '',
+                ],
+            ],
+            [
+                '{@inheritdoc} ok',
+                [
+                    'tag' => 'inheritdoc',
+                    'data' => null,
+                    'text' => 'ok',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * covers ::appendLine
+     */
+    public function testAppendLine()
+    {
+        $blank = new Blank('tag', 'one');
+        $blank->appendLine('  two');
+        $blank->appendLine('three');
+        $this->assertSame("one\n  two\nthree", $blank->text);
+    }
+}
